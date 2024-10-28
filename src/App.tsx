6028d9e713978b16a2f8e2f97be8fecc0eaaa72c
𝙰𝚋𝚞𝚋𝚊𝚛𝚊𝚔𝚊 𝙼𝚘𝚝𝚊𝚜𝚎𝚖 <146123311+mota6em@users.noreply.github.com>
2024-10-28 12:40:26 +0100

    rewrite the Gird layot

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import { useState } from "react";
 
 
 function App() {
-  return (<Grid templateAreas={{
-      base: `"nav " "main"`,
+  return (
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
-  }}>
-    <GridItem area='nav' bg='gold'>Nav</GridItem>
-    <Show above="lg">
-      <GridItem area='aside' bg='red'>aside</GridItem>
-    </Show>
-    <GridItem area='main' bg='blue'>main</GridItem>
-  </Grid>)
+    }}>
+        <GridItem area={"nav"} bg={"red"}>nav</GridItem>
+        <Show above="lg">
+              <GridItem area={"aside"} bg={"blue"}>aside</GridItem>
+        </Show>  
+        <GridItem area={"main"} bg={"gold"}>main</GridItem>
+    </Grid>
+  )
 }
 
 export default App;
