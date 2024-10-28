a0f5aa4da1997e71392c9b7af01b609f97a0f557
𝙰𝚋𝚞𝚋𝚊𝚛𝚊𝚔𝚊 𝙼𝚘𝚝𝚊𝚜𝚎𝚖 <146123311+mota6em@users.noreply.github.com>
2024-10-28 12:30:39 +0100

    First message

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { useState } from "react";
+
+
+
+function App() {
+  return (<Grid templateAreas={{
+      base: `"nav " "main"`,
+      lg: `"nav nav" "aside main"`
+  }}>
+    <GridItem area='nav' bg='gold'>Nav</GridItem>
+    <Show above="lg">
+      <GridItem area='aside' bg='red'>aside</GridItem>
+    </Show>
+    <GridItem area='main' bg='blue'>main</GridItem>
+  </Grid>)
+}
+
+export default App;
