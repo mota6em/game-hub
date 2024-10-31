bfc60fda1c457f9cb85b9257e7396229c926066a
𝙰𝚋𝚞𝚋𝚊𝚛𝚊𝚔𝚊 𝙼𝚘𝚝𝚊𝚜𝚎𝚖 <146123311+mota6em@users.noreply.github.com>
2024-10-31 14:16:01 +0100

    Clean up the games

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -12,35 +13,41 @@ import getCroppedImageUrl from "../sevices/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre  | null
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({selectedGenre,onSelectGenre} : Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner></Spinner>;
   return (
-    <List>
-      {data.map((g) => (
-        <ListItem paddingY={"5px"} key={g.id}>
-          <HStack>
-            <Image
-              boxSize={"32px"}
-              borderRadius={8}
-              src={getCroppedImageUrl(g.image_background)}
-            ></Image>
-            <Button
-            fontWeight={g.id === selectedGenre?.id ? 'bold' : 'normal'}
-              onClick={() => onSelectGenre(g)}
-              variant={"link"}
-              fontSize="lg"
-            >
-              {g.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize={'2xl'} marginBottom={2}>Genres</Heading>
+      <List>
+        {data.map((g) => (
+          <ListItem paddingY={"5px"} key={g.id}>
+            <HStack>
+              <Image
+                boxSize={"32px"}
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(g.image_background)}
+              ></Image>
+              <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                fontWeight={g.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(g)}
+                variant={"link"}
+                fontSize="lg"
+              >
+                {g.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
