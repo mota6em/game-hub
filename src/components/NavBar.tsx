df77030e70cb3b70bfa21ddc6d846465966a1dd0
𝙰𝚋𝚞𝚋𝚊𝚛𝚊𝚔𝚊 𝙼𝚘𝚝𝚊𝚜𝚎𝚖 <146123311+mota6em@users.noreply.github.com>
2024-10-31 14:03:23 +0100

    Filter Games by searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,21 @@
-import { ColorModeContext, HStack, Image, Text } from '@chakra-ui/react';
-import React from 'react';
-import logo from '../assests/profile.png';
-import ColorModeWitch from './ColorModeWitch';
-import SearchInput from './SearchInput';
-function NavBar() {
-  return (
-    <HStack margin={'10px 15px'}>
-        <Image src={logo} boxSize={'40px'} margin={'3px'}></Image>
-        <SearchInput></SearchInput>
-        <ColorModeWitch/>
-    </HStack>
-  )
+import { ColorModeContext, HStack, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import logo from "../assests/profile.png";
+import ColorModeWitch from "./ColorModeWitch";
+import SearchInput from "./SearchInput";
+
+interface Props {
+  onSearch: (searchText: string) => void;
 }
 
-export default NavBar
+function NavBar({onSearch} : Props) {
+  return (
+    <HStack margin={"10px 15px"}>
+      <Image src={logo} boxSize={"40px"} margin={"3px"}></Image>
+      <SearchInput onSearch={onSearch}></SearchInput>
+      <ColorModeWitch />
+    </HStack>
+  );
+}
+
+export default NavBar;
