df53d21c01616b65f633e7f5020871fd421d422a
Your Name <your-email@example.com>
2025-01-28 17:09:49 +0100

    Refactor GameGrid component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,31 +8,29 @@ interface Props {
   isDarkMode: boolean;
   gameQuery: GameQuery;
 }
-const GameGrid = ({ isDarkMode,gameQuery }: Props) => {
+const GameGrid = ({ isDarkMode, gameQuery }: Props) => {
   const { data, error, isLoading } = useGame(gameQuery);
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25,
   ];
+  if (error) return <p>{error} </p>;
   return (
-    <>
-      {error && <p>{error} </p>}
-      <div className="container-fulid">
-        <div className="row d-flex justify-content-center">
-          {isLoading &&
-            skeletons.map((skeleton) => (
-              <GameCardContainer key={skeleton} isDarkMode={isDarkMode}>
-                <GameCardSkeleton />
-              </GameCardContainer>
-            ))}
-          {data.map((game) => (
-            <GameCardContainer key={game.id} isDarkMode={isDarkMode}>
-              <GameCard isDarkMode={isDarkMode} game={game} />
+    <div className="container-fulid">
+      <div className="row d-flex justify-content-center">
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton} isDarkMode={isDarkMode}>
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        </div>
+        {data.map((game) => (
+          <GameCardContainer key={game.id} isDarkMode={isDarkMode}>
+            <GameCard isDarkMode={isDarkMode} game={game} />
+          </GameCardContainer>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
