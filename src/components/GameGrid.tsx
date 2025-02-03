7f7d0b5e66c3bd86aef9d4d86452e7411e468422
Your Name <your-email@example.com>
2025-02-03 23:42:01 +0100

    Fetching infinite scroling using useInfiniteQuery hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { GameQuery } from "../App";
 import useGame from "../hooks/useGames";
 import GameCard from "./GameCard";
@@ -9,28 +10,52 @@ interface Props {
   gameQuery: GameQuery;
 }
 const GameGrid = ({ isDarkMode, gameQuery }: Props) => {
-  const { data, error, isLoading } = useGame(gameQuery);
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGame(gameQuery);
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25,
   ];
   if (error) return <p>{error.message} </p>;
   return (
-    <div className="container-fulid">
-      <div className="row d-flex justify-content-center">
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton} isDarkMode={isDarkMode}>
-              <GameCardSkeleton />
-            </GameCardContainer>
+    <>
+      <div className="container-fulid">
+        <div className="row d-flex justify-content-center">
+          {isLoading &&
+            skeletons.map((skeleton) => (
+              <GameCardContainer key={skeleton} isDarkMode={isDarkMode}>
+                <GameCardSkeleton />
+              </GameCardContainer>
+            ))}
+          {data?.pages.map((page) => (
+            <React.Fragment key={page.next}>
+              {page.results.map((game) => (
+                <GameCardContainer key={game.id} isDarkMode={isDarkMode}>
+                  <GameCard isDarkMode={isDarkMode} game={game} />
+                </GameCardContainer>
+              ))}
+            </React.Fragment>
           ))}
-        {data?.results.map((game) => (
-          <GameCardContainer key={game.id} isDarkMode={isDarkMode}>
-            <GameCard isDarkMode={isDarkMode} game={game} />
-          </GameCardContainer>
-        ))}
+        </div>
+        <div className="d-flex justify-content-start mx-5 my-3">
+          {hasNextPage && (
+            <button
+            className="btn btn-warning fw-bold"
+              disabled={isFetchingNextPage}
+              onClick={() => fetchNextPage()}
+            >
+              {isFetchingNextPage ? "Loading" : "Load More"}
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,19 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Game } from "../assets/ts/types";
 import APICLIENT, { FetchResponse } from "../services/api-client";
 const apiClient = new APICLIENT<Game>("/games");
 const useGame = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () => apiClient.getAll({
-      params: {
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortSelector,
-        search: gameQuery.search,
-      },
-    }),
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortSelector,
+          search: gameQuery.search,
+          page: pageParam,
+        },
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : null;
+    },
   });
 
 export default useGame;
