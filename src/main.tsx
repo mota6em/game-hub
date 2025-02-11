39cb4ad161cedff09ace4842600b81d45fa4d6b9
Your Name <your-email@example.com>
2025-02-11 18:33:52 +0100

    Refactor:  resolve dropdown list issue on platform and order selector by importing missing dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
